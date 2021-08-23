cd6fb36713cdc3d9e4de44fc3c1dc5de1fa932e6
Mehedi390 <mh0816150@gmail.com>
2021-08-22 22:57:06 -0700

    template string using

---
+++ telplate-string.js
@@ -0,0 +1,11 @@
+const multiline = `this is line 1
+ this is line  2 
+ this is line 3
+ this  is line 4
+ this is line 5 
+ this is line 6`
+console.log(multiline);
+const fast = 'mehedi';
+const last = 'hasan';
+const fulName = `${fast} ${last}`;
+console.log(fulName);
