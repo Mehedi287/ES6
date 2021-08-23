2badb2858848232aa5ffddbfc27d67bde4461825
Mehedi390 <mh0816150@gmail.com>
2021-08-23 00:41:08 -0700

    arrow function

---
+++ arrow-big.js
@@ -0,0 +1,17 @@
+const add = (num1, num3) => num1 + num3;
+// console.log(add (41,1));
+const addMultiply = (num1, num2, num3) => num1 + num2 + num3;
+// console.log(addMultiply(85, 4, 1));
+const mathOpreation = () => {
+    const num1 = 89;
+    const num2 = 4;
+    const result = num1 + num2;
+    return result;
+}
+// console.log(mathOpreation());
+// single paramter 
+const s = num => num * 5;
+console.log(s(10));
+// anonumus function in arrow function 
+const a = () => 'brandon sam';
+console.log(a());
