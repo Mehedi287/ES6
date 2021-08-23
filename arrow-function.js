8322e0b7a24f812ef556e73e23f0703544e4d391
Mehedi390 <mh0816150@gmail.com>
2021-08-22 23:50:08 -0700

    arrow function and anonumus function

---
+++ arrow-function.js
@@ -0,0 +1,11 @@
+function add(num1, num2) {
+    return num1 + num2;
+
+}
+console.log(add(4, 4));
+const anonumusFunction = function (num1, num) {
+    return num1 + num;
+}
+console.log(anonumusFunction(5, 5));
+const arrowFuncton = (num1, num2) => num1 + num2;
+console.log(arrowFuncton(6, 6));
